dfea3c4663fa7a6afc6e02fdc3bacb6e62b2b149
Alexandre galvão <alexlealgalvao@gmail.com>
2020-03-08 12:38:50 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=42):
+    def __init__(self, *filhos, nome=None, idade=42):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if  __name__ == '__main__':
-    p = Pessoa('Erica')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    erica = Pessoa(nome='Erica')
+    alex = Pessoa(erica, nome='Alex')
+    print(Pessoa.cumprimentar(erica))
+    print(id(erica))
+    print(erica.cumprimentar())
 
-    print(p.nome)
-    p.nome = 'Alexandre'
-    print(p.nome)
+    print(erica.nome)
+    erica.nome = 'Alexandre'
+    print(erica.nome)
+    print(erica.idade)
+    for filho in alex.filhos:
+        print(filho.nome)
